a7fb2fc67fa9173092bb8d0e30badaf13735c0af
Bizarro <bizarro1306@gmail.com>
2022-02-03 23:58:51 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
